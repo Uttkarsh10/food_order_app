d8011b25883a43fe93dc1958a6b2e251f044aa20
Uttkarsh <uttkarshsaini10@gmail.com>
2023-07-26 00:35:20 +0530

    Added Checkout.js and css files

---
+++ src/Components/Cart/Checkout.js
@@ -0,0 +1,36 @@
+import styles from './Checkout.module.css';
+
+const Checkout = (props) => {
+    
+    const confirmHandler = (event) => {
+        event.preventDefault();
+    }
+
+    return(
+        <form onSubmit={confirmHandler}>
+            <div className={styles.control}>
+                <label htmlFor='name'>Your Name</label>
+                <input type='text' id='name'/>
+            </div>
+
+            <div className={styles.control}>
+                <label htmlFor='street'>Street</label>
+                <input type='text' id='street'/>
+            </div>
+
+            <div className={styles.control}>
+                <label htmlFor='postal'>Postal Code</label>
+                <input type='text' id='postal'/>
+            </div>
+
+            <div className={styles.control}>
+                <label htmlFor='city'>City</label>
+                <input type='text' id='city'/>
+            </div>
+            <button type="button" onClick={props.onCancel}>Cancel</button>
+            <button>Confirm</button>
+        </form>
+    )
+}
+
+export default Checkout;
