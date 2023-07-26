9d8cad827913d51875f287972a9767dc6fcb29a8
Uttkarsh <uttkarshsaini10@gmail.com>
2023-07-26 15:02:38 +0530

    Added Checkout form validity and POST request to send user data to backend

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -8,6 +8,8 @@ import Checkout from './Checkout';
 function Cart(props) {
 
     const [isCheckout,setIsCheckout] = useState(false);
+    const [isSubmitting,setIsSubmitting] = useState(false);
+    const [didSubmit,setDidSubmit] = useState(false);
 
     const cartCtx = useContext(CartContext);
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
@@ -15,6 +17,20 @@ function Cart(props) {
     const cartItemAddHandler = item => {cartCtx.addItem({...item,amount:1})}
     const cartItemRemoveHandler = id => {cartCtx.removeItem(id)}
     const orderHandler = () => {setIsCheckout(true);}
+
+    const submitOrderHandler = async (userData) => {
+        setIsSubmitting(true);
+        await fetch('https://react-http-eb7dd-default-rtdb.firebaseio.com/orders.json',{
+            method:'POST',
+            body: JSON.stringify({
+                user:userData,
+                orderedItems: cartCtx.items
+            })
+        });
+        setIsSubmitting(false); 
+        setDidSubmit(true);
+        cartCtx.clearCart();
+    }
 
     const cartItems = (
         <ul className={styles['cart-items']}>
@@ -29,16 +45,31 @@ function Cart(props) {
                             <button className={styles['button-alt']} onClick={props.closeCart}>Close</button>
                             {hasItems && <button className={styles.button} onClick={orderHandler}>Order</button>}
                         </div>
+
+    const cartModalContent = <React.Fragment>
+                                {cartItems}
+                                    <div className={styles.total}>
+                                        <span>Total Amount</span>
+                                        <span>{totalAmount}</span>
+                                    </div>
+                                {isCheckout && <Checkout onConfirm ={submitOrderHandler} onCancel={props.onClose}/>}
+                                {!isCheckout && modalActions}
+                             </React.Fragment>
+
+    const isSubmittingModalContent = <p>Sending Order Data</p>
+
+    const didSubmitModalContent = <React.Fragment>
+        <p>Successfully sent the order!</p>
+        <div className={styles.actions}>
+            <button className={styles.button} onClick={props.closeCart}>Close</button>
+        </div>
+    </React.Fragment>
         
   return (
     <Modal closeCart={props.closeCart}>
-        {cartItems}
-        <div className={styles.total}>
-            <span>Total Amount</span>
-            <span>{totalAmount}</span>
-        </div>
-        {isCheckout && <Checkout onCancel={props.onClose}/>}
-        {!isCheckout && modalActions}
+        {!isSubmitting && !didSubmit && cartModalContent}
+        {isSubmitting && isSubmittingModalContent}
+        {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   )
 }
--- src/Components/Cart/Checkout.js
+++ src/Components/Cart/Checkout.js
@@ -1,36 +1,88 @@
-import styles from './Checkout.module.css';
+import { useRef, useState } from 'react';
+import classes from './Checkout.module.css';
 
+const isEmpty = (value) => value.trim() === '';
+const isFiveChars = value => value.trim().length === 5;
+ 
 const Checkout = (props) => {
-    
-    const confirmHandler = (event) => {
-        event.preventDefault();
-    }
 
-    return(
-        <form onSubmit={confirmHandler}>
-            <div className={styles.control}>
-                <label htmlFor='name'>Your Name</label>
-                <input type='text' id='name'/>
-            </div>
+    const [formInputsValidity, setFormInputsValidity] = useState({
+        name:true,
+        city:true,
+        street:true,
+        postalCode:true
+    });
 
-            <div className={styles.control}>
-                <label htmlFor='street'>Street</label>
-                <input type='text' id='street'/>
-            </div>
+    const nameInputRef = useRef();
+    const streetInputRef = useRef();
+    const postalCodeInputRef = useRef();
+    const cityInputRef = useRef();
 
-            <div className={styles.control}>
-                <label htmlFor='postal'>Postal Code</label>
-                <input type='text' id='postal'/>
-            </div>
+  const confirmHandler = (event) => {
+    event.preventDefault();
 
-            <div className={styles.control}>
-                <label htmlFor='city'>City</label>
-                <input type='text' id='city'/>
-            </div>
-            <button type="button" onClick={props.onCancel}>Cancel</button>
-            <button>Confirm</button>
-        </form>
-    )
-}
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+    const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
+
+    setFormInputsValidity({
+        name:enteredNameIsValid,
+        street:enteredStreetIsValid,
+        city:enteredCityIsValid,
+        postalCode:enteredPostalCodeIsValid
+    })
+
+    const formIsValid = enteredNameIsValid && enteredStreetIsValid && enteredCityIsValid && enteredPostalCodeIsValid;
+    if(!formIsValid) return;
+    props.onConfirm({
+        name:enteredName,
+        street:enteredStreet,
+        city:enteredCity,
+        postalCode:enteredPostalCode,
+    });
+  };
+
+  const nameControlClasses = `${classes.control} ${formInputsValidity.name ? '': classes.invalid}`;
+  const streetControlClasses = `${classes.control} ${formInputsValidity.street ? '': classes.invalid}`;
+  const cityControlClasses = `${classes.control} ${formInputsValidity.city ? '': classes.invalid}`;
+  const postalCodeControlClasses = `${classes.control} ${formInputsValidity.postalCode ? '': classes.invalid}`;
+
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={nameControlClasses}>
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name' ref={nameInputRef}/>
+        {!formInputsValidity.name && <p>Enter a Valid name</p>}
+      </div>
+      <div className={streetControlClasses}>
+        <label htmlFor='street'>Street</label>
+        <input type='text' id='street' ref={streetInputRef}/>
+        {!formInputsValidity.street && <p>Enter a Valid Street name</p>}
+      </div>
+      <div className={postalCodeControlClasses}>
+        <label htmlFor='postal'>Postal Code</label>
+        <input type='text' id='postal' ref={postalCodeInputRef}/>
+        {!formInputsValidity.postalCode && <p>Enter a Valid Postal Code</p>}
+      </div>
+      <div className={cityControlClasses}>
+        <label htmlFor='city'>City</label>
+        <input type='text' id='city' ref={cityInputRef}/>
+        {!formInputsValidity.city && <p>Enter a Valid City name</p>}
+      </div>
+      <div className={classes.actions}>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
+};
 
 export default Checkout;
